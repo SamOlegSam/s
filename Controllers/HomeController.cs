858b60d143448dccfd0cd24d8e47e8ce16aa3e3d
Oleg <sov@transoil.gomel.by>
2021-03-19 13:19:42 +0300

    s

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using s.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace s.Controllers
+{
+    public class HomeController : Controller
+    {
+        public GENERALEntities db = new GENERALEntities();
+        public PhoneEntities db1 = new PhoneEntities();
+
+        public ActionResult BirthDay()
+        {
+
+            List<C_get_ok_days3> birthday = new List<C_get_ok_days3>();
+            //birthday = db.C_get_ok_days3.OrderBy(f => f.datbegin.Month).ThenBy(g => g.datbegin.Day).ToList();
+            birthday = db.C_get_ok_days3.OrderBy(h=>h.fio).ToList();
+            ViewBag.vvv = birthday;
+            return View(birthday);
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Phone()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+               
+        public ActionResult Vac()
+        {
+            return View();
+        }
+
+        public ActionResult Corruption()
+        {
+            string[] files = Directory.GetFiles(@"\\s\c$\Inetpub\wwwroot\DOCUMENTS\CORRUPTION\", "*", SearchOption.AllDirectories);
+            string[] files1 = Directory.GetDirectories(@"\\s\c$\Inetpub\wwwroot\DOCUMENTS\CORRUPTION\", "*", SearchOption.AllDirectories);
+            
+            var list1 = new List<string>(Directory.GetFiles(@"\\s\c$\Inetpub\wwwroot\DOCUMENTS\CORRUPTION\"));
+            var list2 = new List<string>(Directory.GetDirectories(@"\\s\c$\Inetpub\wwwroot\DOCUMENTS\CORRUPTION\"));
+            var list3 = list2.Concat(list1);
+            ViewBag.fil = files;
+            ViewBag.fil1 = files1;
+            ViewBag.li = list3;
+            return View(list3);
+        }
+        public ActionResult CorruptionPath(string str)
+        {           
+            var list1 = new List<string>(Directory.GetFiles(str));
+            var list2 = new List<string>(Directory.GetDirectories(str));
+            var list3 = list1.Concat(list2);
+            
+            ViewBag.li = list3;
+            return View(list3);
+        }
+
+        public ActionResult GetLastName(string str)
+        {
+            List<C_get_ok_days3> phoneList = new List<C_get_ok_days3>();
+            phoneList = db.C_get_ok_days3.Where(p => p.fio.Contains(str) || p.doljn.Contains(str)).ToList();
+            ViewBag.phone = phoneList;
+            return View(phoneList);
+        }
+        public ActionResult GetLastName1(string str)
+        {
+            List<Phone> phoneList = new List<Phone>();
+            phoneList = db1.Phone.Where(p => p.FIO.Contains(str) || p.Doljnost.Contains(str)).ToList();
+            ViewBag.phone = phoneList;
+            return View(phoneList);
+        }
+    }
+}
---
+++ Scripts/s.js
@@ -0,0 +1,123 @@
+﻿
+//function Export(str) {
+//    var stringhref = "/Home/CorruptionPath?";
+
+//    stringhref += str;
+//    //window.open(stringhref, '_blank');
+//    window.location = stringhref;
+//    //window.location = "/Home/Export/";
+//}
+
+function Export(str) {
+    $.ajax({
+        url: "/Home/CorruptionPath/" + str,
+        type: "GET",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify(),
+        dataType: "html",
+        success: function (result) {
+            $('#ServicesModalContent').html(result);
+            $('#ServicesModal').modal('show');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+
+    })
+}
+
+function GetLastName() {
+
+    var isValid = true;
+
+
+    if ($('#SearchName').val() == "") {
+        $('#SearchName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#SearchName').css('border-color', 'lightgrey');
+    }
+
+
+    if (isValid == false) {
+        return false;
+    }
+
+    var data = {
+        //'ID': ID,
+        'str': $('#SearchName').val()
+
+    };
+
+    $("#search").html("");
+
+    var x = document.getElementById("loadImg");
+    x.style.display = "block";
+
+    $.ajax({
+        url: "/Home/GetLastName",
+        type: "POST",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify(data),
+        dataType: "html",
+        success: function (data) {
+            x.style.display = "none";
+            $('#search').hide();
+            $('#search').html(data).animate({ opacity: 'show' }, "slow");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+
+}
+
+function GetLastName1() {
+
+    var isValid = true;
+
+    if ($('#SearchName').val() == "") {
+        $('#SearchName').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#SearchName').css('border-color', 'lightgrey');
+    }
+
+
+
+    if (isValid == false) {
+        return false;
+    }
+
+
+    var data = {
+
+        'str': $('#SearchName').val(),
+
+    };
+    $("#search").html("");
+
+    var x = document.getElementById("loadImg");
+    x.style.display = "block";
+
+    $.ajax({
+
+        url: "/Home/GetLastName1",
+        type: "POST",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify(data),
+        dataType: "html",
+        success: function (data) {
+            //$('#search').replaceWith(data);
+            x.style.display = "none";
+            $('#search').hide();
+            $('#search').html(data).animate({ opacity: 'show' }, "slow");
+
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
